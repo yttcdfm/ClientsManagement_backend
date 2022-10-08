685753e4a6240178e966b397fd51ce384b70000a
yttcdfm <ytt.akbakb84@gmail.com>
2022-10-08 21:44:45 +0900

    localhost:3000は許可

--- src/main/java/com/sample/controller/ClientController.java
+++ src/main/java/com/sample/controller/ClientController.java
@@ -4,15 +4,17 @@ import com.sample.domain.model.Client;
 import com.sample.domain.service.ClientService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 顧客情報管理いえい.
+ * 顧客情報管理.
  *
  * @author yttak
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ClientController {
   @Autowired
   private ClientService service;
