7a7c8147b6a45a0c97ae2223618b81a2f88ca0b6
yttcdfm <ytt.akbakb84@gmail.com>
2022-10-08 11:17:42 +0900

    checkstyle対応、ClientController側だけ

--- src/main/java/com/sample/BackendApplication.java
+++ src/main/java/com/sample/BackendApplication.java
@@ -3,11 +3,17 @@ package com.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * BackendApplication.
+ *
+ * @author yttak
+ *
+ */
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 
 }
---
+++ src/main/java/com/sample/controller/ClientController.java
@@ -0,0 +1,17 @@
+package com.sample.controller;
+
+/**
+ * 顧客情報管理.
+ *
+ * @author yttak
+ */
+public class ClientController {
+  /**
+   * 顧客情報を全て返す.
+   *
+   * @return APIの戻り値
+   */
+  public String getAll() {
+    return "";
+  }
+}
